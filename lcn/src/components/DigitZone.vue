501421ce2eda847af139e54a0dad50726018e28c
Arno_2T <arnaud.tortora@gmail.com>
2018-08-07 23:01:01 +0200

    display of the digital zone

--- lcn/src/components/DigitZone.vue
+++ lcn/src/components/DigitZone.vue
@@ -1,10 +1,29 @@
 <template>
-    <div>DIGIT ZONE</div>
+    <div id="digit" :digit="digit">
+        {{ digit[0] }} {{digit[1]}}
+    </div>
 </template>
 
 <script>
 export default {
-    
+    data(){
+        return{
+            digit:[],
+        }
+    },
+    created(){
+        document.addEventListener('keydown', this.digitListener1)
+        document.addEventListener('keydown', this.digitListener2)
+
+    },
+    methods:{
+        digitListener1(event){
+            console.log(event)
+            if((event.keyCode >= 48 && event.keyCode <= 57)||(event.keyCode>=96 && event.keyCode<=105)){
+                this.digit.push(event.key)
+            }
+        },
+    }
 }
 </script>
 
@@ -18,6 +37,13 @@ export default {
         height: 200px;
         width: 200px;
         border: 3px solid white;
+    }
+
+    #digit{
+        font-size: 50px;
+        font-weight: bolder;
+        text-align: center;
+        line-height: 4em;
     }
 
 </style>
