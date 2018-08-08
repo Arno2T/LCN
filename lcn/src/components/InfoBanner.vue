d9667ee8203b2744fcacd0f4a1461bbb832e01f9
Eddy Struillou <eddy.s25@hotmail.fr>
2018-08-08 17:05:55 +0200

    Ajout de la vignette et placement des élements

--- lcn/src/components/InfoBanner.vue
+++ lcn/src/components/InfoBanner.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="info">
-    Info Banner
+    <div class="tib">
+      <div class="i">
+        <img src="../images/baleine.jpg"/>
+      </div>
+      <div class="tb">
+        <h3>Titre du programme en cours</h3>
+        <span>Barre de prog</span>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -13,9 +21,33 @@ export default {
 <style lang="less" scoped>
 .info {
   position: absolute;
-  top: 700px;
+  top: 680px;
   height: 20vh;
   width: 99vw;
-  background-color: red;
+  font-family: Verdana, sans-serif;
+  background:linear-gradient(260deg, rgba(255,0,0,0.5) 0%, rgba(255,255,255,0.5) 25%, rgba(255,255,255,0.5) 26%, rgba(128,128,128,0.5) 100%);
+  .tib {
+    display: flex;
+    flex-direction: row;
+    width: 800px;
+    height: 170px;
+    margin-left: 20%;
+  }
+  .i {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 170px;
+    width: 300px;
+  }
+  .tb {
+    flex-direction: column;
+    width: 500px;
+    height: 170px;
+  }
+  img {
+    width: 250;
+    height: 150px;
+  }
 }
 </style>
