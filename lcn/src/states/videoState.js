f384964858583ae39d6e00168f5303c58a6a9d07
Eddy Struillou <eddy.s25@hotmail.fr>
2018-08-28 08:57:09 +0200

    Progress-bar update, mise en place avec le watch

---
+++ lcn/src/states/videoState.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import { EventBus } from '../main'
+
+export let videoStates = new Vue({
+  data: {
+    curTime: 0
+  },
+  watch: {
+    curTime: function () {
+      if (this.curTime) {
+        EventBus.$emit('dataChange', this.curTime)
+      } else {
+        EventBus.$emit('En pause', this.curTime)
+      }
+    }
+  }
+})
