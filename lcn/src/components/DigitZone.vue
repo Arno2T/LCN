15aa45339e316134d0afc42811ba4fb1c20909f1
Arno_2T <arnaud.tortora@gmail.com>
2018-09-04 16:47:39 +0200

    correction de conflits

--- lcn/src/components/DigitZone.vue
+++ lcn/src/components/DigitZone.vue
@@ -1,4 +1,4 @@
-
+<template>
     <div id="digit" :digit="digit" :channel="channel">
         {{ channel }}
     </div>
@@ -20,102 +20,36 @@ export default {
     document.addEventListener('keydown', this.showChannel) //listen digit controls
   },
   methods: {
-    showChannel (event) {
-      if ((event.keyCode >= 48 && event.keyCode <= 57) || (event.keyCode >= 96 && event.keyCode <= 105)) { // numpad & keybord buttons
-        if (!this.elapsed) { clearTimeout(this.timer) }
-        this.digit += event.key
-        this.elapsed = false
-        if (this.digit.length >= 2) {
-          this.channel = this.digit[this.digit.length - 2] + this.digit[this.digit.length - 1]
-          this.timer = setTimeout(() => {
-            this.channelStates.channelRequest = this.channel
-            this.elapsed = true
-            this.digit = ''
-            this.channel = ''
-            this.tune()
-            return this.channelStates.channelRequest
-          }, 2000)
-        } else {
-          this.channel = this.digit
-          this.timer = setTimeout(() => {
-            this.elapsed = true
-            this.channelStates.channelRequest = this.channel
-            this.digit = ''
-            this.channel = ''
-            console.log(this.channelStates.channelRequest)
-            return this.channelStates.channelRequest
-          }, 2000)
-        }
-      }
-    }
-  }
-
-}
-</script>
-
-<style lang="less" scoped>
-    div {
-        font-family: Arial, Helvetica, sans-serif;
-        position: absolute;
-        top:500px;
-        left: 300px;
-        color: white;
-        height: 200px;
-        width: 200px;
-    }
-
-    #digit{
-        font-size: 50px;
-        font-weight: bolder;
-        text-align: center;
-        line-height: 4em;
-    }
-
-</style>
-
-    <div id="digit" :digit="digit" :channel="channel">
-        {{ channel }}
-    </div>
-</template>
-
-<script>
-import { channelStates } from '../states/channelStates.js'
-export default {
-  data () {
-    return {
-      digit: '',
-      channel: '',
-      elapsed: null, //boolean
-      timer: null,
-      channelStates
-    }
-  },
-  created () {
-    document.addEventListener('keydown', this.showChannel) //listen digit controls
-  },
-  methods: {
+    // get channel request and transmit response
     async tune(){
       const response =  await fetch('./data/channel.json')
       const results = await response.json()
-      results.find((element)=>{
-          if ((element.id == this.channelStates.channelRequest) && (this.channelStates.channelRequest)) { 
-            console.log(element.id)
-             this.channelStates.channelResponse = {
-              id: element.id,
-              chaine: element.chaine,
-              src: element.src,
-              videoId: element.videoId,
-              miniature: element.miniature,
-              programme: element.programme,
-              duree: element.duree
-            }
-           
-            return this.channelStates.channelResponse
-          }
-          else{
-            return console.log("Chaine non définie")
-          }
+      const channelSearch = results.find((element) => {
+        return element.id == this.channelStates.channelRequest
       })
+      if (channelSearch){
+         this.channelStates.channelResponse = {
+                id: channelSearch.id,
+                chaine: channelSearch.chaine,
+                src: channelSearch.src,
+                videoId: channelSearch.videoId,
+                miniature: channelSearch.miniature,
+                programme: channelSearch.programme,
+                duree: channelSearch.duree
+            }
+            return this.channelStates.channelResponse
+      }
+      else{
+                return this.channelStates.channelResponse ={
+                  id: null,
+                  chaine: null,
+                  src:null,
+                  videoId: null,
+                  miniature: null,
+                  programme: null,
+                  duree: null
+                }
+              }
     },
     showChannel (event) {
       if ((event.keyCode >= 48 && event.keyCode <= 57) || (event.keyCode >= 96 && event.keyCode <= 105)) { // numpad & keybord buttons
@@ -139,16 +73,13 @@ export default {
             this.channelStates.channelRequest = this.channel
             this.digit = ''
             this.channel = ''
-            console.log(this.channelStates.channelRequest)
             this.tune()
-            console.log(this.channelStates.channelRequest)
             return this.channelStates.channelRequest
           }, 2000)
         }
       }
     }
   }
-
 }
 </script>
 
@@ -162,12 +93,10 @@ export default {
         height: 200px;
         width: 200px;
     }
-
     #digit{
         font-size: 50px;
         font-weight: bolder;
         text-align: center;
         line-height: 4em;
     }
-
 </style>
--- lcn/src/states/channelStates.js
+++ lcn/src/states/channelStates.js
@@ -1,5 +1,13 @@
 export const channelStates = {
 
   channelRequest: null,
-  channelResponse: null
+  channelResponse: {
+                id: 1,
+                chaine: "TF1",
+                src: "./assets/logoChaines/tf1.png",
+                videoId: "http://mazwai.com/system/posts/videos/000/000/192/original/le-temps-dun-paris.mp4?1445792574",
+                miniature: "./assets/miniatures/paris.jpg",
+                programme: "Ici c'est Paris",
+                duree: 180
+  }
 }
