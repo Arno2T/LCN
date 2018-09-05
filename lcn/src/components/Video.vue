1d1285ab37834bf3294c5568c36793ec43395394
Eddy Struillou <eddy.s25@hotmail.fr>
2018-09-05 12:21:01 +0200

    Affichage heure de début et heure de fin du programme

--- lcn/src/components/Video.vue
+++ lcn/src/components/Video.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="player">
-    <video id="media-video" :src="this.channelStates.channelResponse.videoId" controls autoplay loop muted>{{ refreshTime() }}</video>
+    <video id="media-video" :src="this.channelStates.channelResponse.videoId" controls autoplay loop muted>{{ refreshTime() }} {{ timeVideoStart() }}</video>
   </div>
 </template>
 
@@ -28,7 +28,16 @@ export default {
     refreshTime () {
       setInterval(this.SelectVideo, 1000)
     },
-
+    //Va envoyer l'heure à laquelle le programme a démarré
+    timeVideoStart () {
+      const moment = require('moment')
+      videoStates.vidStart = moment().format('HH:mm');
+    },
+    //Va envoyer la durée du programme dans le states
+    videoDuration () {
+      const video = document.querySelector('#media-video')
+      videoStates.vidDuration = video.duration
+    }
   },
 }
 </script>
