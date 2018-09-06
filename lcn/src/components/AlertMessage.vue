e238b2e888c28ac200f2cc6f6bebc7dec8f64f31
Arno_2T <arnaud.tortora@gmail.com>
2018-09-06 14:24:57 +0200

    alertMessage ok

--- lcn/src/components/AlertMessage.vue
+++ lcn/src/components/AlertMessage.vue
@@ -1,73 +1,70 @@
 <template>
     <div class="alert" >
-        <p :alert="alert()" :show="show()">{{ this.message }}</p>
+        <h2 :alert="alert()">{{ this.message }}</h2>
     </div>
 </template>
 <script>
-import { channelStates } from "../states/channelStates"
+import { channelStates } from "../states/channelStates";
 export default {
   data() {
     return {
       channelStates,
       message: null
-    }
+    };
   },
   created() {
-    document.addEventListener("keydown", this.close)
+    document.addEventListener("keydown", this.close);
   },
   mounted() {
-    setTimeout(this.hide, 5000)
+    setTimeout(this.hide, 5000);
   },
   methods: {
     alert() {
       if (this.channelStates.channelResponse === false)
-        this.message = "Cette chaîne n'est pas définie"
+        this.message = "Cette chaîne n'est pas définie";
     },
     close(event) {
-      const box = document.querySelector(".alert")
+      const box = document.querySelector(".alert");
       if (box && event.keyCode === 27) {
-        this.hide()
-      }
-    },
-    show() {
-      if (this.channelStates.channelResponse === false) {
-        const box = document.querySelector(".alert")
-        box.style.display = "block"
+        this.hide();
       }
     },
     hide() {
-      const box = document.querySelector(".alert")
-      box.style.display = "none"
+      this.channelStates.channelResponse=true
     }
   },
-  watch: {
-    'channelStates': function(response) {
-      console.log(response)
-
-      if (response) {
-        console.log("hide")
-      } else {
-        console.log("show")
-      }
-    }
-  }
-}
+};
 </script>
 
 <style>
 .alert {
   height: 10%;
-  width: 30%;
+  width: 40%;
   border: 19x solid black;
   border-radius: 5px;
-  box-shadow: 0, 0, 5px, 5px, black;
-  background-color: #ffefef;
+  box-shadow: 0 0 5px 5px white;
+  background: linear-gradient(
+    260deg,
+    rgba(255, 0, 0, 0.8) 0%,
+    rgba(255, 255, 255, 0.8) 25%,
+    rgba(255, 255, 255, 0.8) 26%,
+    rgba(128, 128, 128, 0.8) 100%
+  );
   text-align: center;
-  line-height: 3em;
+  white-space: nowrap;
+  line-height: 1em;
+  font-weight: bold;
+  text-shadow: 1px 1px black;
+  color: white;
+  font-size: 30px;
+  font-family: Arial, Helvetica, sans-serif;
   position: absolute;
   display: block;
   margin: auto;
-  top: 100px;
-  left: 100px;
+  top: 30%;
+  left: 30%;
+}
+.show {
+  display: none;
 }
 </style>
