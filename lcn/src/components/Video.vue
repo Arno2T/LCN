2a4868a39e4559d9d85c5408486278f6811f8f87
Eddy Struillou <eddy.s25@hotmail.fr>
2018-09-24 11:16:11 +0200

    delete bug and clean code base

--- lcn/src/components/Video.vue
+++ lcn/src/components/Video.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="player">
-    <video id="media-video" :src="this.channelStates.channelResponse.videoId" controls autoplay loop muted>{{ refreshTime() }} {{ timeVideoStart() }}</video>
+    <video id="media-video" :src="this.channelStates.channelResponse.videoId" autoplay loop muted>{{ refreshTime() }} {{ timeVideoStart() }}</video>
   </div>
 </template>
 
@@ -32,11 +32,6 @@ export default {
     timeVideoStart () {
       const moment = require('moment')
       videoStates.vidStart = moment().format('HH:mm');
-    },
-    //Va envoyer la durée du programme dans le states
-    videoDuration () {
-      const video = document.querySelector('#media-video')
-      videoStates.vidDuration = video.duration
     }
   },
 }
